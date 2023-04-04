e9cd494bc25488ea10bbc0a705a32db5bbdef0aa
Feni Kamushipu <fkamushipu@gmail,com>
2023-04-04 11:59:25 +0200

    Build bug fix

--- next.config.js
+++ next.config.js
@@ -2,3 +2,30 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+
+const path = require('path');
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Disable file-system routing
+    config.devServer = {
+      historyApiFallback: true,
+    };
+
+    // Add aliases for component imports
+    config.resolve.alias['@components'] = path.join(__dirname, 'components');
+    config.resolve.alias['@lib'] = path.join(__dirname, 'lib');
+
+    // Exclude SVG files from file-loader
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    // Return modified configuration
+    return config;
+  },
+};
+
+
