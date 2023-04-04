c1e5698abf3de12e424cbb590165b9d46a9e7ec7
Feni Kamushipu <fkamushipu@gmail,com>
2023-04-04 12:43:55 +0200

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,31 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-}
-
-
-const path = require('path');
-
-module.exports = {
-  webpack: (config, { isServer }) => {
-    // Disable file-system routing
-    config.devServer = {
-      historyApiFallback: true,
-    };
-
-    // Add aliases for component imports
-    config.resolve.alias['@components'] = path.join(__dirname, 'components');
-    config.resolve.alias['@lib'] = path.join(__dirname, 'lib');
-
-    // Exclude SVG files from file-loader
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-
-    // Return modified configuration
+  reactStrictMode: false,
+  webpack5: true,
+  webpack :(config) => {
+    config.resolve.fallback ={ fs: false};
     return config;
   },
 };
-
-
