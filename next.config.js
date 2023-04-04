a1df45c301963e9f05ebe4baea8438cf82f84a10
Feni Kamushipu <fkamushipu@gmail,com>
2023-04-04 12:47:09 +0200

    Remove webpack5 option from config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
-  webpack5: true,
   webpack :(config) => {
     config.resolve.fallback ={ fs: false};
     return config;
